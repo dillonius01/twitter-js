06f9bc0805a7c7d98cf794ddb3e7c6a8c54990dc
Dillon Powers <dillon.c.powers@gmail.com>
2016-09-19 18:00:14 -0400

    sections 1 to 5

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+
 var app = express(); // creates instance of express application
 app.use(morgan('combined'));
+app.engine('html', nunjucks.render); // when giving html to res.render, tells it to use nunjucks
+
+nunjucks.configure('views', { noCache: true }); // points nunjucks to dir with all templates
+app.set('view engine', 'html'); // tells res.render to work with html files
+
+
 
 // app.use('/', function(req, res, next) {
 // 	console.log(req.path + req.method);
 // 	next();
 // })
 
+
+app.get('/people', function(req, res, next) {
+	res.render('index', {'title': 'Test Title', 'people': [{'name': 'Dillon'}, {'name': 'Jake'}]});
+})
 
 
 app.get('/', function(req, res, next) {
