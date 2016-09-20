75d21ad6737ff4bde8c36899ac53367e83e6f139
Jake Peyser <jakepeyser@gmail.com>
2016-09-20 10:26:18 -0400

    sdf

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ app.engine('html', nunjucks.render); // when giving html to res.render, tells it
 nunjucks.configure('views', { noCache: true }); // points nunjucks to dir with all templates
 app.set('view engine', 'html'); // tells res.render to work with html files
 
-
-
+// Logging for every request
 // app.use('/', function(req, res, next) {
 // 	console.log(req.path + req.method);
 // 	next();
 // })
-
 
 app.get('/people', function(req, res, next) {
 	res.render('index', {'title': 'Test Title', 'people': [{'name': 'Dillon'}, {'name': 'Jake'}]});
